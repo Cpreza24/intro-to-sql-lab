31b1bc3621eb5acbc6259b205e0c62605992d209
Cesar Preza <cpreza93@yahoo.com>
2025-04-08 19:11:00 -0700

    adds exercises 2-4

--- clues.sql
+++ clues.sql
@@ -2,22 +2,24 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
  
 -- Write SQL query here
-select * from countries where region='Southern Europe' and population=1000;
+select * from countries where region='Southern Europe' order by population asc limit 1;
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
-
+select * from countrylanguages where countrycode='VAT';
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
-
+select countries.code, countries.name, countrylanguages.countrycode, countrylanguages.language, countrylanguages.percentage
+from countries
+inner join countrylanguages on countries.code = countrylanguages.countrycode where countrylanguages.percentage=100 and countrylanguages.language='Italian';
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
-
+select * from cities where countrycode='SMR' and name!='San Marino';
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
